41111f9de2b7342c0efa20173e1d7511bbf0c529
Antonio Pagano <apagano@wawand.co>
2016-06-07 19:32:28 -0500

    [feature] allowing users to create gophers

--- handlers/create.go
+++ handlers/create.go
@@ -1,9 +1,20 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
 
+	"github.com/apaganobeleno/go-heroku-circle/db"
+	"github.com/apaganobeleno/go-heroku-circle/models"
+)
+
+//CreateGopher ... You guess
 func CreateGopher(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	w.WriteHeader(201)
+	DB, _ := db.Connection()
+	gopher := models.Gopher{}
+	gopher.Name = r.Form.Get("Name")
+	gopher.Company = r.Form.Get("Company")
 
+	DB.Create(&gopher)
+	http.Redirect(w, r, "/", 302)
 }
